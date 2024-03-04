a5043ab6c2582b6b7979fff0ab9f31b1f6ed684a
Kiadtisak-PC <kiadtisak.th.64@ubu.ac.th>
2024-03-04 11:31:50 +0700

    อัพเดธเเอดเพจ

--- myproject/admin_app/views.py
+++ myproject/admin_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from form_service.models import ModelForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import user_passes_test
@@ -12,3 +12,19 @@ def adnin_home(req):
     list_user = ModelForm.objects.all()
     return render(req, "admin_home.html", {"list_user": list_user})
 
+
+def admin_order(req):
+    order = ModelForm.objects.all()
+    return render(req,'admin_order.html',{'list_order':order})
+
+def admin_detail(req,id):
+    order = ModelForm.objects.get(pk=id)
+    return render(req,'admin_detail.html',{'order':order})
+
+def update_status(req,id):
+    order = ModelForm.objects.get(pk=id)
+    if req.method == "POST":
+        order.status = req.POST['status']
+        order.save()
+        return redirect("/admin_home/admin_order")
+        
--- myproject/form_service/models.py
+++ myproject/form_service/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+    
+ORDER_CHOICE = (
+    ("1","เสร็จเเล้ว"),
+    ("2","ยังไม่เสร็จ"),
+    ("3","รอการส่งซัก"),
+)
 class ModelForm(models.Model):
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length = 100)
@@ -10,5 +17,9 @@ class ModelForm(models.Model):
     date_start = models.DateField()
     date_end = models.DateField()
     clothes = models.CharField(max_length = 100)
-    number_baskets = models.IntegerField(default = 0)
+    number_clothes = models.CharField(max_length = 100 , null = True)
+    number_baskets = models.CharField(max_length = 100 , null = True)
     note = models.CharField(max_length = 100)
+    status = models.CharField(max_length=255,choices=ORDER_CHOICE,default=3)
+    
+
